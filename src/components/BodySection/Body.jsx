190e854df5adafe9f483887513617283d53e323d
Ishana0 <ishanakalikiri01@gmailcom>
2024-01-18 18:03:05 +0530

    components

---
+++ src/components/BodySection/Body.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./body.css"
+const Body = (props) => {
+    console.log(props.data)
+    return (
+        <>
+            <div>
+                {props.data.map((image) => {
+                    return (
+                        <div key={image.id} className="column">
+                            <img src={image.img} alt=""></img>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+}
+export default Body;
